4977f7fa12e77928297b641d8ebc3dec4dac1397
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-12-10 16:38:08 -0300

    Realização da atividade sobre quadrante no plano cartesiano.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO.Compression;
+
+Console.WriteLine("--- Quadrante Cartesiano ---");
+
+Console.WriteLine("Entre com o ponto desejado.");
+
+Console.Write("Coordenada x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Coordenada y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x == 0 && y == 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica na origem.");
+} else if (x == 0 || y == 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica no eixo das ordenadas.");
+} else if (x > 0 && y > 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 1.");
+} else if (x < 0 && y > 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 2.");
+} else if (x < 0 && y < 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 3.");
+} else if (x > 0 && y < 0) {
+  Console.WriteLine($"O ponto ({x}, {y}) fica no quadrante 4.");
+}
